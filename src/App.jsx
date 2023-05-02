d05b2ecd90793dd50396cb39157277fadb0663da
Your Name~ <gmaturan60@gmail.com>
2023-05-02 14:30:57 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import {
   Route,
   Redirect,
   Switch,
+  useHistory,
 } from "react-router-dom";
 
 //user context usestate
@@ -23,6 +24,7 @@ import "react-toastify/dist/ReactToastify.css";
 import SideBar from "./components/SideBar";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Notfound from "./pages/Notfound";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import User from "./pages/User";
@@ -38,6 +40,10 @@ import Lupon_v3 from "./pages/lupon_v3/lupon";
 import { getLoggedInUser } from "./api/user";
 
 const App = () => {
+  const history = useHistory();
+
+  // then add this to the function that is called for re-rendering
+  history.go(0);
   const [user, setUser] = useState(null);
 
   //preventing lossing the user data
